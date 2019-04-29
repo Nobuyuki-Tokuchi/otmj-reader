69ee02cd279559a22a0876c54a5bb8ab75e7d177
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2019-04-29 12:31:06 +0900

    不要ファイル削除

--- main/script/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-window.addEventListener("load", () => {
-    const viewer = new OtmViewer();
-    ko.applyBindings(viewer, document.getElementById("main"));
-});
-//# sourceMappingURL=index.js.map
